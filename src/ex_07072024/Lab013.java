d3f6f4468298d6647ca2b64ec4eb448cb8b37c47
ankitamca03 <ankitaaggarwal30@gmail.com>
2024-07-23 17:06:32 +0530

    All Files added

---
+++ src/ex_07072024/Lab013.java
@@ -0,0 +1,14 @@
+package ex_07072024;
+
+public class Lab013 {
+
+    //Can I run a class without main method??
+    //run option?
+    static public void main(String[] args) {
+
+    }
+
+    public static void main() {
+
+    }
+}
