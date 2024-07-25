6d3fbf23c9b3aba0e6033ae015c6b490377f285f
ankitamca03 <ankitaaggarwal30@gmail.com>
2024-07-25 19:28:27 +0530

    All Files added

---
+++ src/ex_13072024/Lab027.java
@@ -0,0 +1,10 @@
+package ex_13072024;
+
+public class Lab027 {
+    public static void main(String[] args) {
+        int age= 10; //Decimal Number system
+        int b = 0b1010; // Binary Number - Automation no use
+        int octal = 0101; //Octal Number System
+        int hex = 0Xface; //0X is for hex
+    }
+}
