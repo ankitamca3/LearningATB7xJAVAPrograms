2420c3f17f0cc99332834c26580311dcfc736da7
ankitamca03 <ankitaaggarwal30@gmail.com>
2024-07-26 19:24:23 +0530

    All Files added

---
+++ src/ex_14072024/Lab051.java
@@ -0,0 +1,26 @@
+package ex_14072024;
+
+public class Lab051 {
+    // Arithmetic Operators
+    // +,-,%,/,*
+
+    // Compound Assignment Operators
+    // +=, -=, /=, *=
+
+    // Relational Operators
+    // > , < , >= <=, !, != , == (compare two value)
+
+
+    // Logical Operators
+    //  OR or and , || , && , !
+
+    // They will come in the future.  -OOPs concepts
+
+    // new Operator
+    // instanceOf Operator
+
+
+    // Optional to learn - digital Electronics
+    // Bitwise - Not important for Automation
+    // ~ - Bitwise Not, >> - Right shift, << Left Shift, ^ - XOR
+}
