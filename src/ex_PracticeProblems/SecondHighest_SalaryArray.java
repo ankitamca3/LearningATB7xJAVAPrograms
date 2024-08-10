b660ba37621a93b6d575f255bc70ca3b2877dc3b
ankitamca3 <ankitaaggarwal30@gmail.com>
2024-08-10 17:35:45 +0530

    Practice program update!!

--- src/ex_PracticeProblems/SecondHighest_SalaryArray.java
+++ src/ex_PracticeProblems/SecondHighest_SalaryArray.java
@@ -4,20 +4,21 @@ public class SecondHighest_SalaryArray {
     public static void main(String[] args) {
         int[] salaries = {7, 10, 4, 13, 25, 18, 9, 8, 17};
 
-//        int max = salaries[0];
-       int max = 1;
-        int secMax = 1;
-        for (int i = 0; i < salaries.length ; i++) {
-            if( salaries[i] > max){
-                max = salaries[i];
-                secMax = max;
-            }
-            else if(salaries[i]>=secMax){
-                secMax = salaries[i];
+       int temp;
+       int size = salaries.length;
+
+        for (int i = 0; i < size ; i++) {
+
+            for (int j = i+1; j < size ; j++){
+
+                if(salaries[i]>salaries[j]){
+                    temp = salaries[i];
+                    salaries[i] = salaries[j];
+                    salaries[j] = temp;
+                }
             }
         }
 
-        System.out.println(max);
-        System.out.println(secMax);
+        System.out.println("Second Highest Salary :" +salaries[size-2]);
     }
 }
